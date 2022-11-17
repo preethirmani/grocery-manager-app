0340a2c4e6178a72e6b4904ce2dafb81ae06ee70
Preethi <writetopreethir@gmail.com>
2022-11-17 15:56:47 -0800

    Project Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';  
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroceryComponent } from './grocery/grocery.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    CommonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/grocery/grocery.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grocery',
+  templateUrl: './grocery.component.html',
+  styleUrls: ['./grocery.component.css']
+})
+export class GroceryComponent implements OnInit {
+ item = {
+    id : 0,
+    name : '',
+    check : false
+   
+  };
+ // item = '';
+
+   items: any[] = [];
+
+   
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+   
+  onClick() {
+   
+    this.item.id = new Date().getTime();
+    this.item.check = false;
+    this.items.push(this.item);
+
+    //clearing item object 
+   this.item = {
+      id : 0,
+      name : '',
+      check : false
+    }
+    
+    console.log('item obj cleared');
+    console.log('this.item.id:'+this.item.id);
+    console.log('this.item.name:'+this.item.name);
+  }
+
+  onDelete(item:any) {
+    const start = this.items.indexOf(item);
+    this.items.splice(start, 1);
+    console.log(this.items);
+  }
+
+  onCheck(item: any) {
+    const indx = this.items.indexOf(item);
+    this.items[indx].check = true;
+    
+  }
+}
